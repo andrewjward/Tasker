716c842eb52eb5436c98bfa0dc45fd57c00af78f
Andrew <a.drew517@gmail.com>
2023-03-07 13:04:03 -0500

    Feature 13 complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -16,15 +16,3 @@ class SignUpForm(forms.Form):
         max_length=150,
         widget=forms.PasswordInput,
     )
-
-
-class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(
-        max_length=150,
-        widget=forms.PasswordInput,
-    )
-    password_confirmation = forms.CharField(
-        max_length=150,
-        widget=forms.PasswordInput,
-    )
--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from projects.views import projects_list
+from projects.views import projects_list, show_project
 
-urlpatterns = [path("", projects_list, name="list_projects")]
+urlpatterns = [
+    path("", projects_list, name="list_projects"),
+    path("<int:id>/", show_project, name="show_project"),
+]
